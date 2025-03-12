70207485e4d5b3c16dd0d7acfea3a0f82290af1b
Takayuki Nagatomi <tommyt6073@gmail.com>
2025-03-12 22:40:01 +0900

    Add go.mod file parser for extracting direct dependencies

---
+++ parser/gomod.go
@@ -0,0 +1,55 @@
+package parser
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/tnagatomi/gh-go-mod-browser/model"
+	"golang.org/x/mod/modfile"
+)
+
+// GoModParser parses go.mod files and extracts direct dependencies
+type GoModParser struct {
+	filePath string
+}
+
+// NewGoModParser creates a new GoModParser instance
+func NewGoModParser(filePath string) *GoModParser {
+	return &GoModParser{
+		filePath: filePath,
+	}
+}
+
+// ParseCurrentDirectory parses the go.mod file in the current directory
+func ParseCurrentDirectory() (*GoModParser, error) {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+	return NewGoModParser(filepath.Join(cwd, "go.mod")), nil
+}
+
+// Parse parses the go.mod file and returns a list of direct dependencies
+func (p *GoModParser) Parse() ([]*model.Package, error) {
+	data, err := os.ReadFile(p.filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	file, err := modfile.Parse(p.filePath, data, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var packages []*model.Package
+
+	// Add direct requires
+	for _, req := range file.Require {
+		if !req.Indirect {
+			pkg := model.NewPackage(req.Mod.Path, req.Mod.Version)
+			packages = append(packages, pkg)
+		}
+	}
+
+	return packages, nil
+}
